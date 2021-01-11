c9fbfe903b17a2273df9eb076c8c7bf1e1ca7208
Jonathan Senior <jonosenior@gmail.com>
2021-01-11 12:03:40 +0000

    Add package model and associated table

---
+++ db/migrate/20210111112004_create_packages.rb
@@ -0,0 +1,12 @@
+class CreatePackages < ActiveRecord::Migration[6.0]
+  def change
+    create_table :packages do |t|
+      t.string :name
+      t.string :version
+      t.string :r_version_needed
+      t.string :dependencies
+      t.string :license
+      t.string :url
+    end
+  end
+end
